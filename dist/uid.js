2b5978a02cac69013d44dc860d8e64828a0f0ab0
Jareer Abdullah <jforeverything2007@gmail.com>
2022-09-17 10:32:08 +0200

    ES5 > ES6

--- dist/uid.js
+++ dist/uid.js
@@ -5,12 +5,11 @@ function rand(min, max) {
         (max - min + 1)) +
         min);
 }
-var nanoid = function (length, chars) {
-    if (chars === void 0) { chars = '1234567890abcdefghijklmnopqrstuvwxyz'; }
-    var id = [];
-    var charlist = chars.split('');
+const nanoid = (length, chars = '1234567890abcdefghijklmnopqrstuvwxyz') => {
+    let id = [];
+    let charlist = chars.split('');
     while (id.length < length) {
-        var random = charlist[rand(0, charlist.length)];
+        const random = charlist[rand(0, charlist.length)];
         if (id[id.length - 1] != random) {
             id.push(random);
         }
